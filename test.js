28c04662853cd021351719bc75c12014049ac220
copc <102230084@sv1.dut.udn.vn>
2026-03-31 07:50:39 +0700

    Done to create number check file

---
+++ test.js
@@ -0,0 +1,12 @@
+function isOdd(num) {
+    return num % 2 === 1;
+}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+console.log(isOdd(1)); // true
+console.log(isOdd(2)); // false
+console.log(isEven(1)); // false
+console.log(isEven(2)); // true
